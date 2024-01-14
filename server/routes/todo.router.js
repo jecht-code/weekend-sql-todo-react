96bd13bc67f3267f0c30b141a44758ed5d5b3792
Scott Pak <gpak86@gmail.com>
2024-01-14 17:00:45 -0500

    postico GET ROUTE is working

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get('/', (req, res) => {
+    const sqlText = 'SELECT * FROM tasktable ORDER BY task DESC;';
+    pool
+        .query(sqlText)
+        .then((result) => {
+            console.log('get result', result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making DB query ${sqlText}`, error);
+            res.sendStatus(500); // good server always responds
+        });
+});
 // POST
 
 // PUT
